45483a5629c0fbb641260f0ff1e9b152a96ff0e0
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2024-03-25 13:55:35 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let input1;
-let input2;
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
 
 const operators = document.querySelectorAll('.calc-button');
+
 const plusButton = document.getElementById("plusButton");
 const minusButton = document.getElementById("minusButton");
 const multiplyButton = document.getElementById("multiplyButton");
@@ -12,30 +13,32 @@ const resultField = document.getElementById('resultField');
 operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
         let result;
-        updateInputs();
-        if (isNaN(input1) || isNaN(input2)){
-            result = 'NaN'
-        } else if (e.target === plusButton){
-            result = parseInt(input1) + parseInt(input2);
-        } else if (e.target === minusButton){
-            result = input1 - input2;
-        } else if (e.target === multiplyButton){
-            result = input1 * input2;
-        } else if (e.target === divideButton){
-            result = input1 / input2;
+
+        switch (e.target) {
+            case plusButton: {result = parseInt(input1.value) + parseInt(input2.value); break;}
+            case minusButton: {result = input1.value - input2.value; break;}
+            case multiplyButton: {result = input1.value * input2.value; break;}
+            case divideButton: {result = input1.value / input2.value; break;}
         }
     
-        if (!isNaN(result) && result % Math.floor(result) != 0 
-        && (Math.floor(result) == 0 || !isNaN(result % Math.floor(result))))
+        if (!isNaN(result) && result % 1 !== 0)
             result = result.toFixed(3);
 
-        if (input1 == '' || input2 == '') result = 'Enter values';
+        if (input1.value == '' || input2.value == '') result = 'Enter values first';
 
         resultField.innerHTML = result;
     })
 })
 
-function updateInputs() {
-    input1 = document.getElementById("input1").value;
-    input2 = document.getElementById("input2").value;
+input1.addEventListener('input', checkInput);
+input2.addEventListener('input', checkInput);
+
+function checkInput(e) {
+    const {value, classList} = e.target;
+    classList.toggle('wrong-input', isNaN(value))
+
+    document.querySelectorAll('.warning').forEach(label => {
+        if (label.getAttribute('for') == e.target.getAttribute('id'))
+            label.classList.toggle('hidden', !isNaN(value));
+    })
 }
